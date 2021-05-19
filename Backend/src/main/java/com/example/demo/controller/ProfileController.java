f184082e588834145f9d5e45111566f8e60c860f
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2021-05-19 15:04:55 +0200

    Profile crud backend

--- Backend/src/main/java/com/example/demo/controller/ProfileController.java
+++ Backend/src/main/java/com/example/demo/controller/ProfileController.java
@@ -1,9 +1,57 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.Profile;
+import com.example.demo.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProfileController {
 
+    @Autowired
+    ProfileRepository profileRepository;
 
+    //    ==================================================== GET Activity ================================================
+
+    //    ====== SELECT ALL ACTIVITIES =====
+    @GetMapping("/select/profile/about")
+    public List<Profile> getProfileAbout() {
+        List<Profile> profiles = profileRepository.findAll();
+
+        return profiles;
+
+    }
+
+    //    ==================================================== POST PROFILES ================================================
+
+//    @PostMapping(value="/insert/profile/about", consumes = "application/json")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Profile insertProfileAbout(@RequestBody Profile profile){
+//
+//        return profileRepository.save(profile);
+//
+//    }
+
+    @PostMapping(value="/edit/profile/about", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Profile editProfileAbout(@RequestBody Profile profile){
+
+        return profileRepository.save(profile);
+
+    }
+//    ==================================================== DELETE PROFILES =============================================
+
+//    @ResponseStatus(code=HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/delete/activity/{id}")
+//    public void deleteActivity(@PathVariable int id){
+//        try {
+//            profileRepository.deleteById(id);
+//        } catch (EmptyResultDataAccessException ex) {
+//            System.out.println("FEJL i DELETE =" + ex.getMessage());
+//        }
+//    }
 }
--- Backend/src/main/java/com/example/demo/repository/ProfileRepository.java
+++ Backend/src/main/java/com/example/demo/repository/ProfileRepository.java
@@ -4,4 +4,5 @@ import com.example.demo.model.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class ProfileRepository extends JpaRepository<Profile, Integer> {
+
 }

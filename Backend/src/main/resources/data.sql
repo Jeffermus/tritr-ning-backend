ae268bad438e298ed2a5b9a93583baea2cfee232
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2021-05-20 11:59:36 +0200

    Blog crud backend

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,1 +1,4 @@
 INSERT INTO blog_page (id, title, description, img, datetime, author) VALUES (1 , 'Golf', 'Kun Hole in ones', 'img', '26-04-2021', 'Torben Træben');
+INSERT INTO blog_page (id, title, description, img, datetime, author) VALUES (2 , 'Golf by Night', 'Kun Hole in ones', 'img', '28-04-2021', 'Jens Træben');
+INSERT INTO blog_page (id, title, description, img, datetime, author) VALUES (3 , 'Golf by Morning', 'Kun Hole in ones', 'img', '26-04-2021', 'Thor Træben');
+

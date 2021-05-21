794257323a26f940a9d2e933a034d25f741ffb50
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2021-05-21 11:21:47 +0200

    Activity crud backend

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,1 +1,5 @@
-INSERT INTO activity_page (title, description, img, banner) VALUES ('Styrketræning', 'Tobias bænker 982 kg', 'img', 'banner');
+INSERT INTO activity_page (id, title, description, img, banner) VALUES (1, 'Styrketræning', 'Tobias bænker 982 kg', 'img', 'banner');
+INSERT INTO activity_page (id, title, description, img, banner) VALUES (2, 'Kondi', 'Inteval', 'img', 'banner');
+INSERT INTO activity_page (id, title, description, img, banner) VALUES (3, 'Massage', 'Ømme Muskler', 'img', 'banner');
+INSERT INTO activity_page (id, title, description, img, banner) VALUES (4, 'Kostplan', 'Get Fit Food', 'img', 'banner');
+

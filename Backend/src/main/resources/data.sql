6c250904e9ad74dd0abc89ca31bd32ab3441ae2b
Rewax <daniel.lorenzen@hotmail.com>
2021-05-25 18:11:43 +0200

    Review Backend done

--- Backend/src/main/resources/data.sql
+++ Backend/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 -- INSERT INTO blog_page (id, title, description, img, datetime, author) VALUES (1 , 'Golf', 'Kun Hole in ones', 'img', '26-04-2021', 'Torben Træben');
-INSERT INTO review (author, description, review_image) VALUES ('Daniel', 'Daniel er super god til reviews', 'img');
+INSERT INTO review (author, description, review_image) VALUES ('Daniel', 'Hvorfor sletter du ikke', 'img');
 INSERT INTO pages (title, description, img, banner) VALUES ('Styrketræning', 'Tobias bænker 982 kg', 'img', 'banner');
 INSERT INTO pages (title, description, img, banner) VALUES ('Kondi', 'Inteval', 'img', 'banner');
 INSERT INTO pages (title, description, img, banner) VALUES ('Massage', 'Ømme Muskler', 'img', 'banner');

747ff47e56a14e9a5186d2838894cbcc29b847bd
SoSoCho <70580037+SoSoCho@users.noreply.github.com>
2021-05-27 14:32:20 +0200

    update v.2

--- Backend/src/main/java/com/example/demo/controller/UserController.java
+++ Backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,75 +1,47 @@
-//package com.example.demo.controller;
-//
-//import com.example.demo.model.Auth;
-//import com.example.demo.model.Users;
-//import com.example.demo.repository.AuthRepository;
-//import org.apache.catalina.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.dao.EmptyResultDataAccessException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//public class UserController {
-//
-//    @Autowired
-//    UserRepository userRepository;
-//
-//    @Autowired
-//    AuthRepository authRepository;
-//
-//
-////    ==================================================== GET USERS ===================================================
-//
-//    //     === SELECT ALL USERS ===
-//    @GetMapping(value="/select/users")
-//    public List<Users> getAllUsers(){
-//
-//        List <Users> user = userRepository.findAll();
-//
-//        System.out.println("USER FOUND==="+user);
-//
-//        return user;
-//    }
-//
-////   ==================================================== POST USERS TO DB =============================================
-//
-//    //     === INSERT USER ===
-//    @PostMapping(value = "/insert/user", consumes = "application/json")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public Users insertUser(@RequestBody Users users) {
-//        System.out.println(users);
-//
-//        return userRepository.save(users);
-//    }
-//
-//    //     === INSERT AUTH ===
-//    @PostMapping(value = "/insert/auth", consumes = "application/json")
-//    @ResponseStatus(HttpStatus.CREATED)
-//    public Auth insertAuth(@RequestBody Auth auth) {
-//        System.out.println(auth);
-//
-//        return authRepository.save(auth);
-//    }
-//
-//    //     === UPDATE ONE PROFILE ===
-//    @PostMapping(value = "/update/user", consumes = "application/json")
-//    @ResponseStatus(HttpStatus.OK)
-//    public void updateUserCredentials(@RequestBody Users users) {
-//        System.out.println("USERS ==="+users);
-//        User oneUser = userRepository.findById(users.getId());
-//        oneUser.setMail(users.getMail());
-//        if(users.getPassword() != null){
-//            oneUser.setPassword(users.getPassword());
-//        }
-//        System.out.println("ONE USER ==="+oneUser);
-//        userRepository.save(oneUser);
-//    }
-//
-////    ==================================================== DELETE USER ==============================================
-//
+package com.example.demo.controller;
+
+import com.example.demo.model.Users;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+//    ==================================================== GET USERS ===================================================
+
+    //     === SELECT ALL USERS ===
+    @GetMapping(value="/select/users")
+    public List<Users> getAllUsers(){
+
+        List <Users> user = userRepository.findAll();
+
+        System.out.println("USER FOUND==="+user);
+
+        return user;
+    }
+
+//   ==================================================== POST USERS TO DB =============================================
+
+    //     === INSERT USER ===
+    @PostMapping(value = "/insert/user", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Users insertUser(@RequestBody Users users) {
+        System.out.println(users);
+
+        return userRepository.save(users);
+    }
+
+
+//    ==================================================== DELETE USER ==============================================
+
 //    @ResponseStatus(code=HttpStatus.OK)
 //    @PostMapping("/delete/user/{id}")
 //    public void deleteUser(@PathVariable int id){
@@ -80,6 +52,6 @@
 //            System.out.println("FEJL i DELETE =" + ex.getMessage());
 //        }
 //    }
-//
-//
-//}
+
+
+}
